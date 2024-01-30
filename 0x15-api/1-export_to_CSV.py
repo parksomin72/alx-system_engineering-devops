f9cb9cc1a68fdf6ba58540e656cb2a94c222a64b
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-29 17:35:03 -0800

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,7 +11,29 @@ users_url = "https://jsonplaceholder.typicode.com/users?id="
 todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def user_info(id):
+def check_number_of_tasks(id):
+    """ Check number of tasks in CSV """
+
+    total_tasks = 0
+    response = requests.get(todos_url).json()
+    for i in response:
+        if i['userId'] == id:
+            total_tasks += 1
+
+    num_lines = 0
+    with open(str(id) + ".csv", 'r') as f:
+        csv_reader = csv.reader(f)
+        header = next(csv_reader)  # Skip the header
+        for _ in csv_reader:
+            num_lines += 1
+
+    if total_tasks == num_lines:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
+
+
+def check_user_info(id):
     """ Check user information """
 
     response = requests.get(users_url + str(id)).json()
@@ -28,5 +50,38 @@ def user_info(id):
     print("User ID and Username: OK")
 
 
+def check_output_formatting(id):
+    """ Check CSV formatting """
+
+    response = requests.get(todos_url).json()
+    with open(str(id) + ".csv", 'r') as f:
+        csv_reader = csv.reader(f)
+        output = list(csv_reader)
+
+        count = 0
+        flag = 0
+        for i in response:
+            if i['userId'] == id:
+                url = users_url + str(i['userId'])
+                usr_resp = requests.get(url).json()
+                line = [str(i['userId']), usr_resp[0]['username'], str(i['completed']), i['title']]
+                count += 1
+                if line not in output:
+                    print("Task {} Formatting: Incorrect".format(count))
+                    flag = 1
+
+        if flag == 0:
+            print("Formatting: OK")
+
+
 if __name__ == "__main__":
-    user_info(int(sys.argv[1]))
+    user_id = int(sys.argv[1])
+
+    # Check number of tasks
+    check_number_of_tasks(user_id)
+
+    # Check user info
+    check_user_info(user_id)
+
+    # Check output formatting
+    check_output_formatting(user_id)
