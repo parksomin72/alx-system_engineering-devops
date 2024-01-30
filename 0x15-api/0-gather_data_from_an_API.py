158ed6655deaeafda350e52bd284b959bd3fab70
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-29 16:41:11 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,31 @@
 import requests
 from sys import argv
 
+def fetch_user_data(employee_id):
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    return user_response.json()
+
+def fetch_todo_data(employee_id):
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    return todos_response.json()
 
 if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: ./0-gather_data_from_an_API.py <employee id>")
         exit()
-    user = requests.get(
-                     "https://jsonplaceholder.typicode.com/users/{}"
-                     .format(argv[1])
-                    ).json()
-    if not user:
+
+    employee_id = argv[1]
+    user_data = fetch_user_data(employee_id)
+
+    if not user_data:
         exit()
     else:
-        name = user.get('name')
-    all = requests.get(
-                     "https://jsonplaceholder.typicode.com/todos?userId={}"
-                     .format(argv[1])
-                    ).json()
-    dones = [i for i in all if i.get('completed')]
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(dones), len(all)))
-    for d in dones:
-        print("\t {}".format(d.get('title')))
+        employee_name = user_data.get('name')
+
+    todos_data = fetch_todo_data(employee_id)
+    completed_tasks = [task['title'] for task in todos_data if task['completed']]
+
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todos_data)}):")
+    
+    for task_title in completed_tasks:
+        print(f"\t {task_title}")
