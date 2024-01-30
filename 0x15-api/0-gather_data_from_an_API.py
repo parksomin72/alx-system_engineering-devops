d1bc35c3f283147a6835f5f5b9fb86d87321ec44
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-29 17:02:17 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,13 +4,20 @@
 import requests
 from sys import argv
 
+
 def fetch_user_data(employee_id):
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    user_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    )
     return user_response.json()
 
+
 def fetch_todo_data(employee_id):
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
     return todos_response.json()
+
 
 if __name__ == "__main__":
     if len(argv) != 2:
@@ -26,9 +33,11 @@ if __name__ == "__main__":
         employee_name = user_data.get('name')
 
     todos_data = fetch_todo_data(employee_id)
-    completed_tasks = [task['title'] for task in todos_data if task['completed']]
+    completed_tasks = [
+    task['title'] for task in todos_data if task['completed']]
 
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todos_data)}):")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({len(completed_tasks)}/{len(todos_data)}):")
 
     for task_title in completed_tasks:
         print(f"\t {task_title}")
