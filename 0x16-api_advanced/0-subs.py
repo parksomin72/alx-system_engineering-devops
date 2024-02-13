2d96525263d0a760d36a4bd35078b58590127f97
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-13 08:53:32 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,30 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Custom User-Agent header to prevent Too Many Requests error
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    print("Response status code:", response.status_code)  # Debugging
 
-# Test the function
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            return data['subscribers']
+    return 0
+
 if __name__ == '__main__':
-    subreddit = input("Enter subreddit name: ")
-    print(number_of_subscribers(subreddit))
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print(number_of_subscribers(sys.argv[1]))
