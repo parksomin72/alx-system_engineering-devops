1a62823553217d2cdc3c7670914a3b243a55cb67
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-13 07:44:13 -0800

    Top Ten

---
+++ 1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles of the first 10
+    hot posts listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            children = data.get('children')
+            if children:
+                for post in children:
+                    print(post.get('data').get('title'))
+    else:
+        print(None)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
