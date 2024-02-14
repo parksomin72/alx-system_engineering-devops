2bac2d7caa8f5f677300d7e8e9c2f93b08a38343
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-14 19:20:20 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,12 @@
 # Puppet manifest to fix Apache 500 error
 #
-# Description: This Puppet manifest fixes the Apache 500 error by resolving the identified issue.
-# Author: [Your Name]
+# Description: This Puppet manifest fixes the Apache 500 error
+# by resolving the identified issue.
+# Author: Fatima Zahrae El hand
 
 # Define an exec resource to fix the identified issue
 exec { 'fix-wordpress':
-  command     => 'your-fix-command-here', # Replace with the actual command to fix the issue
+  command     => 'actual-command-to-fix-the-issue',
   path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   refreshonly => true,
 }
