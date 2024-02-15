fe8773c9ba9c5c8d0085bf12e70c0d2a5414fdb3
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-15 12:15:17 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,22 @@
-# Puppet manifest to fix Apache 500 error
-#
-# Description: This Puppet manifest fixes the Apache 500 error
-# by resolving the identified issue.
-# Author: Fatima Zahrae El hand
+# Puppet manifest to fix Apache returning the correct page
 
-# Define an exec resource to fix the identified issue
-exec { 'fix-wordpress':
-  command     => 'actual-command-to-fix-the-issue',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
+# Execute commands to fix the issue
+exec { 'fix_apache':
+  command     => 'service apache2 restart', # Restart Apache service
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,                      # Only execute when notified
 }
 
-# Notify service restart if the fix is applied
+# Notify the command to execute when Apache is restarted
 service { 'apache2':
   ensure  => running,
-  enable  => true,
-  require => Exec['fix-wordpress'],
+  require => Exec['fix_apache'],  # Ensure Apache is restarted after fix command execution
+}
+
+# Define a file resource to ensure the correct page is served
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "<html><head><title>Apache is serving the correct page</title></head><body><h1>Welcome to Apache!</h1><p>This is the correct page served by Apache.</p></body></html>",
+  mode    => '0644',  # Set appropriate permissions
+  require => Service['apache2'],  # Ensure Apache is running before updating the page
 }
