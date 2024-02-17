c090397a7543673b262cd7f8780d1b9779a2f2d5
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-17 13:15:56 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,14 @@
-# Puppet manifest to fix Apache returning the correct page
+# 0-strace_is_your_friend.pp
 
-# Execute commands to fix the issue
-exec { 'fix-apache-error':
-  command     => '/bin/sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf',
+# Fix for Apache returning a 500 error
+exec { 'fix-apache-500-error':
+  command     => '/bin/sed -i "s/LogLevel warn/LogLevel debug/g" /etc/apache2/apache2.conf',
   path        => '/bin',
   refreshonly => true,
 }
 
-# Notify the command to execute when Apache is restar
 service { 'apache2':
   ensure    => running,
   enable    => true,
-  subscribe => Exec['fix-apache-error'],
-}
-
-# Define a file resource to ensure the correct page is served
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "<html><head><title>Apache is serving the correct page</title></head><body><h1>Welcome to Apache!</h1><p>This is the correct page served by Apache.</p></body></html>",
-  mode    => '0644',  # Set appropriate permissions
-  require => Service['apache2'],  # Ensure Apache is running before updating the page
+  subscribe => Exec['fix-apache-500-error'],
 }
