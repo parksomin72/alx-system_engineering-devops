6a8653253d52f0c4db36a492f855e42f4aa51c2d
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-17 13:25:24 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -7,6 +7,21 @@ exec { 'fix-apache-500-error':
   refreshonly => true,
 }
 
+# Configure Apache to serve the correct page
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => '<!DOCTYPE html>
+<html>
+<head>
+  <title>Holberton - Just another WordPress site</title>
+</head>
+<body>
+  <h1>Welcome to Holberton</h1>
+  <p>Yet another bug by a Holberton student</p>
+</body>
+</html>',
+}
+
 service { 'apache2':
   ensure    => running,
   enable    => true,
