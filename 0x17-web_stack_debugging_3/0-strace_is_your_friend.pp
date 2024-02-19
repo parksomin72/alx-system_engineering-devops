44fc59ce0c422514f6c8c6238b4c35f6793cd717
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-19 13:03:45 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,39 +1,14 @@
 # 0-strace_is_your_friend.pp
-# Puppet manifest to fix Apache 500 error and ensure Apache returns a 200 status code
 
-# Fix for Apache returning a 500 error
-exec { 'fix-apache-500-error':
-  command     => '/bin/sed -i "s/LogLevel warn/LogLevel debug/g" /etc/apache2/apache2.conf && service apache2 restart',
-  path        => '/bin',
-  refreshonly => true,
-}
-
-# Ensure Apache is running and enabled
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-# Configure Apache to serve the correct page and verify that it returns a 200 status code
-file { '/var/www/html/index.html':
+# Intentionally cause an error in Apache configuration to return 500 status code
+file { '/etc/apache2/sites-available/000-default.conf':
   ensure  => present,
-  content => '<!DOCTYPE html>
-<html>
-<head>
-  <title>Holberton - Just another WordPress site</title>
-</head>
-<body>
-  <h1>Welcome to Holberton</h1>
-  <p>Yet another bug by a Holberton student</p>
-</body>
-</html>',
+  content => "This is an intentional error to cause Apache to return a 500 status code",
 }
 
-# Verify that Apache returns a 200 status code
-exec { 'verify-apache-status':
-  command     => 'curl -s -o /dev/null -w "%{http_code}" http://localhost/',
-  path        => '/usr/bin',
-  logoutput   => true,
-  unless      => 'curl -s -o /dev/null http://localhost/ || false',
-  subscribe   => Service['apache2'],
+# Restart Apache to apply changes
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  require   => File['/etc/apache2/sites-available/000-default.conf'],
 }
