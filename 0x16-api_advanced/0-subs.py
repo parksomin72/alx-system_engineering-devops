eee194e267ac019fd1f310d624a925cb4cc8fdf0
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-28 02:21:44 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Function to query subscribers on a give Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
 
