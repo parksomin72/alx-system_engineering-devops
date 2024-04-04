cd10f1b1ab212688720869056dd5a8554d3e282b
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-04 04:38:06 +0000

    new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,20 +9,25 @@ def number_of_subscribers(subreddit):
     headers = {
         "User_Agent": "Linux:0x16.api.advance:v1.0.0 (by /u/parksomin72)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
     try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
         data = response.json().get("data")
         if data is not None:
             return data.get("subscribers", 0)
         else:
             return 0
-    except ValueError:
-        return 0
+    except requests.HTTPError as e:
+        print("HTTP Error: {} - {}".format(e.response.status_code, e.response.reason))
+    except requests.RequestException as e:
+        print("Request Exception: {}".format(e))
+    except ValueError as e:
+        print("JSON Decoding Error: {}".format(e))
+    return 0  # Return 0 for any encountered errors
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subscribers = number_of_subscribers(sys.argv[1])
+        print("{:d}".format(subscribers))
